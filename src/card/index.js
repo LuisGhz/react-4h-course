a9540f7e73fc5cc3bf7872fc8d253efca2414c2c
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-23 13:17:35 -0500

    Create props argument. Show props.name

--- src/card/index.js
+++ src/card/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './index.css';
 
-export function Card() {
+export function Card(props) {
   return (
     <div className="card">
       <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: '100%' }} />
       <div className="container">
-        <h4><b>John Doe</b></h4>
+        <h4><b>{ props.name }</b></h4>
         <p>Architect & Engineer</p>
       </div>
     </div>
