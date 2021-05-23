5274fb91e4e1ea7be886a4f20bc107111929de4c
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-23 12:51:42 -0500

    Set new avatar url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="card">
-        <img src="img_avatar.png" alt="Avatar" style={{ width: '100%' }} />
+        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style={{ width: '100%' }} />
         <div className="container">
           <h4><b>John Doe</b></h4> 
           <p>Architect & Engineer</p> 
