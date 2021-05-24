b42a30d8d4bf2ec3fd6e79337a8633df604df54e
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-24 15:14:35 -0500

    Set state to show or hide card

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React, { useState } from 'react';
 import faker from 'faker';
 import { Card } from 'card/index';
 import { YesNoButtons } from 'yes-no-buttons/index';
+import './App.css';
 
 function App() {
   const [name, setName] = useState('Edwin');
+  const [showCard, setShowCard] = useState(true);
 
   const changeNameHandler = () => setName( faker.name.firstName() );
 
   const changeNameFromInputHandler = event => setName(event.target.value);
 
+  const toggleCard = () => setShowCard(!showCard);
+
   return (
     <div className="App">
-      <Card 
+      <button className="button" onClick={ () => toggleCard() } >Show / Hide card</button>
+      { showCard && <Card 
         avatar={ 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg' }
         name={ name }
         title={ 'Customer Research Developer' }
         onChangeName={ () => changeNameHandler() }
         onChangeNameFromInput={ changeNameFromInputHandler }
-      ><YesNoButtons /></Card>
+      ><YesNoButtons /></Card> }
     </div>
   );
 }
