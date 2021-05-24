113830f5803c975b31fb66e5b03dc2d6ac486979
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-24 14:33:08 -0500

    Delete last two cards. Set a static avatar and title. Set name using useState. Get random name from faker in changeNameHandler function.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+import React, { useState } from 'react';
 import faker from 'faker';
 import { Card } from 'card/index';
 import { YesNoButtons } from 'yes-no-buttons/index';
 
 function App() {
+  const [name, setName] = useState('Edwin');
+
+  const changeNameHandler = () => {
+    setName( faker.name.firstName() );
+  }
+
   return (
     <div className="App">
       <Card 
-        avatar={ faker.image.avatar() }
-        name={ `${ faker.name.firstName() } ${ faker.name.firstName() }` }
-        title={ `${ faker.name.jobTitle() }` }
-      ><YesNoButtons /></Card>
-      <Card 
-        avatar={ faker.image.avatar() }
-        name={ `${ faker.name.firstName() } ${ faker.name.firstName() }` }
-        title={ `${ faker.name.jobTitle() }` }
-      ><YesNoButtons /></Card>
-      <Card 
-        avatar={ faker.image.avatar() }
-        name={ `${ faker.name.firstName() } ${ faker.name.firstName() }` }
-        title={ `${ faker.name.jobTitle() }` }
+        avatar={ 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg' }
+        name={ name }
+        title={ 'Customer Research Developer' }
+        onChangeName={ () => changeNameHandler() }
       ><YesNoButtons /></Card>
     </div>
   );
