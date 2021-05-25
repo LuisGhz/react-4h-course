daf435c4880c3b4bbb8e0d6097899f63a2f71234
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-25 13:42:03 -0500

    Set name directly to cards array. Set cards array properties to Card component Set key using index to Card component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import { Card } from 'card/index';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('Edwin');
   const [showCard, setShowCard] = useState(true);
 
   const [cards, setCard] = useState([
     {
       avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: name,
+      name: 'Edwin',
       title: 'Customer Research Developer'
     },
     {
       avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: name,
+      name: 'Edwin',
       title: 'Customer Research Developer'
     },
     {
       avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: name,
+      name: 'Edwin',
       title: 'Customer Research Developer'
     },
   ]);
@@ -31,11 +30,11 @@ function App() {
     <div className="App">
       <button className="button" onClick={ () => toggleCard() } style={{ 'margin': '1rem 0' }}>Show / Hide card</button>
       { showCard && (
-      cards.map(card => 
-        <Card 
-          avatar={ 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg' }
+      cards.map(({ avatar, name, title }, index) => 
+        <Card key={ index }
+          avatar={ avatar }
           name={ name }
-          title={ 'Customer Research Developer' }
+          title={ title }
         />)
       )}
     </div>
