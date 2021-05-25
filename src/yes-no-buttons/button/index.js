db002180e1287ff071cfb3d77b678661856627a6
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-25 13:28:39 -0500

    Delete YesNoButtons component

--- src/yes-no-buttons/button/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css';
-
-export const Button = props => {
-  return (
-    <button className="single-button" style={{ backgroundColor: props.backgroundColor }}>{ props.textContent }</button>
-  );
-}
