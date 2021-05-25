fac1b88fd5dc7498dffbda1dfd6c1fc28c8f6adc
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-25 14:16:40 -0500

    Set onClick property in button to run onDelete

--- src/card/index.js
+++ src/card/index.js
@@ -7,7 +7,7 @@ export const Card = props => {
       <div className="container">
         <h4><b>{ props.name }</b></h4>
         <p>{ props.title }</p>
-        <p className="center-text"><button className="button button-red" >Delete</button></p>
+        <p className="center-text"><button className="button button-red" onClick={ () => props.onDelete() } >Delete</button></p>
         <div>{ props.children }</div>
       </div>
     </div>
