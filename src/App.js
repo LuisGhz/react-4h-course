a1f19eaa7722a7c056b118fdd07d4ff8e7606bd8
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-27 07:26:23 -0500

    Convert App component to a class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import faker from 'faker';
 import { ThemeProvider } from 'styled-components';
 import { Card } from 'card/index';
@@ -10,64 +10,74 @@ const theme = {
   mango: 'yellow'
 }
 
-function App() {
-  const [showCard, setShowCard] = useState(true);
+class App extends Component {
 
-  const [cards, setCards] = useState([
-    {
-      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: 'Edwin 1',
-      title: 'Customer Research Developer'
-    },
-    {
-      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: 'Edwin 2',
-      title: 'Customer Research Developer'
-    },
-    {
-      avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
-      name: 'Edwin 3',
-      title: 'Customer Research Developer'
-    },
-  ]);
+  constructor(props) {
+    super(props);
 
-  const buttonClasses = ['button']
+    this.state = {
+      cards: [
+        {
+          avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+          name: 'Edwin 1',
+          title: 'Customer Research Developer'
+        },
+        {
+          avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+          name: 'Edwin 2',
+          title: 'Customer Research Developer'
+        },
+        {
+          avatar: 'https://cdn.fakercloud.com/avatars/mwarkentin_128.jpg',
+          name: 'Edwin 3',
+          title: 'Customer Research Developer'
+        },
+      ],
+      showCard: true
+    };
+  }
 
-  if (cards.length < 3) buttonClasses.push('pink-button');
-  if (cards.length < 2) buttonClasses.push('red-button');
-
-  const toggleCard = () => setShowCard(!showCard);
-
-  const deleteCardHandler = (index)  => {
-    const cards_copy = [...cards];
+  
+  toggleCard() { this.setState({ showCard: !this.showCard }) } 
+  
+  deleteCardHandler(index) {
+    const cards_copy = [...this.state.cards];
     cards_copy.splice(index, 1);
-    setCards(cards_copy)
+    this.setState({ cards: cards_copy })
   }
-
-  const changeNameHandler = (event, index) => {
-    const cards_copy = [...cards];
+  
+  changeNameHandler(event, index) {
+    const cards_copy = [...this.state.cards];
     cards_copy[index].name = event.target.value;
-    setCards(cards_copy);
+    this.setState({cards: cards_copy});
+  }
+  
+  render() {
+    const buttonClasses = ['button']
+  
+    if (this.state.cards.length < 3) buttonClasses.push('pink-button');
+    if (this.state.cards.length < 2) buttonClasses.push('red-button');
+
+    return (
+      <ThemeProvider theme={ theme }>
+        <div className="App">
+          <Button color="primary" length={ this.state.cards.length } onClick={ () => this.toggleCard() } >Show Hide</Button>
+          <button className={ buttonClasses.join(' ') } onClick={ () => this.toggleCard() } style={{ 'margin': '1rem 0' } }>Show / Hide card</button>
+          { this.state.showCard && (
+          this.state.cards.map(({ avatar, name, title }, index) => 
+            <Card key={ index }
+              avatar={ avatar }
+              name={ name }
+              title={ title }
+              onDelete={ () => this.deleteCardHandler(index) }
+              onNameChanges={ event =>  this.changeNameHandler(event, index)}
+            />)
+          )}
+        </div>
+      </ThemeProvider>
+    );
   }
 
-  return (
-    <ThemeProvider theme={ theme }>
-      <div className="App">
-        <Button color="primary" length={ cards.length } >Show Hide</Button>
-        <button className={ buttonClasses.join(' ') } onClick={ () => toggleCard() } style={{ 'margin': '1rem 0' } }>Show / Hide card</button>
-        { showCard && (
-        cards.map(({ avatar, name, title }, index) => 
-          <Card key={ index }
-            avatar={ avatar }
-            name={ name }
-            title={ title }
-            onDelete={ () => deleteCardHandler(index) }
-            onNameChanges={ event =>  changeNameHandler(event, index)}
-          />)
-        )}
-      </div>
-    </ThemeProvider>
-  );
 }
 
 export default App;
