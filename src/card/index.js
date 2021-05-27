2a08e441e122215103f0f79e4c7bf4db531c8057
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-27 07:32:38 -0500

    Convert Card component to a class

--- src/card/index.js
+++ src/card/index.js
@@ -1,16 +1,24 @@
+import { Component } from 'react';
 import './index.css';
 
-export const Card = props => {
-  return (
-    <div className="card">
-      <img src={`${ props.avatar }`} alt="Avatar" style={{ width: '100%' }} />
-      <div className="container">
-        <h4><b>{ props.name }</b></h4>
-        <p>{ props.title }</p>
-        <p><input type="text" value={ props.name } onChange={ props.onNameChanges } /></p>
-        <p className="center-text"><button className="button button-red" onClick={ () => props.onDelete() } >Delete</button></p>
-        <div>{ props.children }</div>
+export class Card extends Component {
+
+  render() {
+    return (
+      <div className="card">
+        <img src={`${ this.props.avatar }`} alt="Avatar" style={{ width: '100%' }} />
+        <div className="container">
+          <h4><b>{ this.props.name }</b></h4>
+          <p>{ this.props.title }</p>
+          <p>
+            <input type="text" value={ this.props.name } onChange={ this.props.onNameChanges } />
+          </p>
+          <p className="center-text">
+            <button className="button button-red" onClick={ () => this.props.onDelete() } >Delete</button>
+          </p>
+          <div>{ this.props.children }</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
