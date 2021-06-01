6a37ef90a97d64c7bf7effb84332bf1859b4283a
Luis Gutiérrez <luisghtz@outlook.com>
2021-05-31 20:43:36 -0500

    Delete avatar and change title to phone

--- src/card/index.js
+++ src/card/index.js
@@ -6,10 +6,9 @@ export class Card extends Component {
   render() {
     return (
       <div className="card">
-        <img src={`${ this.props.avatar }`} alt="Avatar" style={{ width: '100%' }} />
         <div className="container">
           <h4><b>{ this.props.name }</b></h4>
-          <p>{ this.props.title }</p>
+          <p>{ this.props.phone }</p>
           <p>
             <input type="text" value={ this.props.name } onChange={ this.props.onNameChanges } />
           </p>
